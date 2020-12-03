c94691356d34c73a407e1e0a8e442921e67c9d51
Fritz Schrogl <fschrogl@users.noreply.github.com>
2020-12-03 20:14:07 +0100

    Solved puzzle Day01 Part 2

--- src/main/java/at/schrogl/aoc/d01/Day01Application.java
+++ src/main/java/at/schrogl/aoc/d01/Day01Application.java
@@ -29,7 +29,7 @@ public class Day01Application {
     }
 
     private void solvePart1(List<Integer> inputList) {
-        for (int i = 0; i < inputList.size(); i++) {
+        for (int i = 0; i < (inputList.size() - 1); i++) {
             int base = inputList.get(i);
             for (Integer next : inputList.subList(i + 1, inputList.size())) {
                 if (base + next == 2020) {
@@ -40,10 +40,28 @@ public class Day01Application {
                 }
             }
         }
+        System.out.println("No solution was found!");
     }
 
     private void solvePart2(List<Integer> inputList) {
-        // TODO
+        for (int i = 0; i < (inputList.size() - 1); i++) {
+            int base = inputList.get(i);
+            List<Integer> inputSublist2 = inputList.subList(i + 1, inputList.size());
+            for (int i1 = 0; i1 < inputSublist2.size(); i1++) {
+                int base2 = inputSublist2.get(i1);
+                if (base + base2 < 2020) {
+                    for (Integer next : inputList.subList(i1 + 1, inputSublist2.size())) {
+                        if (base + base2 + next == 2020) {
+                            System.out.println("Sum: " + base + " + " + base2 + " + " + next + " = " + (base + base2 + next));
+                            System.out.println("Multiply: " + base + " * " + base2 + " * " + next + " = " + (base * base2 * next));
+                            System.out.println();
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("No solution was found!");
     }
 
 }
