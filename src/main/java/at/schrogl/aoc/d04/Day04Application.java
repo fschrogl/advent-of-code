d2cdf2c96cc6c449d2783eb0dce067091780a5f7
Fritz Schrogl <fschrogl@users.noreply.github.com>
2020-12-04 18:30:00 +0100

    Solved puzzle Day04 Part 2

--- src/main/java/at/schrogl/aoc/d04/Day04Application.java
+++ src/main/java/at/schrogl/aoc/d04/Day04Application.java
@@ -26,13 +26,13 @@ public class Day04Application {
             passports.add(Passport.newInstance(batchDataBuilder.toString()));
         }
 
-        long validPassports = passports.stream()
-            .filter(Passport::isValid)
-            .count();
-
         System.out.println("## DAY 04: Part I");
         System.out.printf("Total passports: %d\n", passports.size());
-        System.out.printf("Valid passports: %d\n", validPassports);
+        System.out.printf("Valid passports: %d\n", passports.stream().filter(Passport::isValidPart1).count());
+
+        System.out.println("\n## DAY 04: Part II");
+        System.out.printf("Total passports: %d\n", passports.size());
+        System.out.printf("Valid passports: %d\n", passports.stream().filter(Passport::isValidPart2).count());
     }
 
     private static List<String> getInputLines() throws IOException, URISyntaxException {
@@ -44,6 +44,9 @@ public class Day04Application {
     private static class Passport {
 
         private final Map<PassportField, String> dataFields = new HashMap<>();
+        private final EnumSet<PassportField> requiredFields = EnumSet.of(PassportField.BIRTH_YEAR, PassportField.ISSUE_YEAR,
+            PassportField.EXPIRATION_YEAR, PassportField.HEIGHT, PassportField.HAIR_COLOR, PassportField.EYE_COLOR,
+            PassportField.PASSPORT_ID);
 
         private Passport() {
             // intentionally private
@@ -66,25 +69,75 @@ public class Day04Application {
             return passport;
         }
 
-        private boolean isValid() {
-            EnumSet<PassportField> requiredFields = EnumSet.of(PassportField.BIRTH_YEAR, PassportField.ISSUE_YEAR,
-                PassportField.EXPIRATION_YEAR, PassportField.HEIGHT, PassportField.HAIR_COLOR, PassportField.EYE_COLOR,
-                PassportField.PASSPORT_ID);
-            Set<PassportField> passportFields = dataFields.keySet();
-            return passportFields.containsAll(requiredFields);
+        private boolean isValidPart1() {
+            return dataFields.keySet().containsAll(requiredFields);
+        }
+
+        private boolean isValidPart2() {
+            return isValidPart1() &&
+                dataFields.entrySet().stream()
+                    .allMatch(e -> e.getKey().isValid(e.getValue()));
         }
 
     }
 
-    private enum PassportField {
-        BIRTH_YEAR("byr"),
-        ISSUE_YEAR("iyr"),
-        EXPIRATION_YEAR("eyr"),
-        HEIGHT("hgt"),
-        HAIR_COLOR("hcl"),
-        EYE_COLOR("ecl"),
-        PASSPORT_ID("pid"),
-        COUNTRY_ID("cid");
+    private interface PassportFieldValidation {
+        boolean isValid(String value);
+    }
+
+    private enum PassportField implements PassportFieldValidation {
+        BIRTH_YEAR("byr") {
+            public boolean isValid(String value) {
+                int integer = Integer.parseInt(value.trim());
+                return integer >= 1920 && integer <= 2002;
+            }
+        },
+        ISSUE_YEAR("iyr") {
+            public boolean isValid(String value) {
+                int integer = Integer.parseInt(value.trim());
+                return integer >= 2010 && integer <= 2020;
+            }
+        },
+        EXPIRATION_YEAR("eyr") {
+            public boolean isValid(String value) {
+                int integer = Integer.parseInt(value.trim());
+                return integer >= 2020 && integer <= 2030;
+            }
+        },
+        HEIGHT("hgt") {
+            public boolean isValid(String value) {
+                String trimmedValue = value.trim();
+                if (trimmedValue.endsWith("cm")) {
+                    Integer integer = Integer.parseInt(trimmedValue.substring(0, trimmedValue.length() - 2));
+                    return integer >= 150 && integer <= 193;
+                } else if (trimmedValue.endsWith("in")) {
+                    Integer integer = Integer.parseInt(trimmedValue.substring(0, trimmedValue.length() - 2));
+                    return integer >= 59 && integer <= 76;
+                } else
+                    return false;
+            }
+        },
+        HAIR_COLOR("hcl") {
+            public boolean isValid(String value) {
+                return value.trim().matches("#[a-f0-9]{6}");
+            }
+        },
+        EYE_COLOR("ecl") {
+            public boolean isValid(String value) {
+                String[] validColors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+                return Arrays.stream(validColors).anyMatch(ec -> ec.equalsIgnoreCase(value.trim()));
+            }
+        },
+        PASSPORT_ID("pid") {
+            public boolean isValid(String value) {
+                return value.trim().matches("[0-9]{9}");
+            }
+        },
+        COUNTRY_ID("cid") {
+            public boolean isValid(String value) {
+                return true;
+            }
+        };
 
         private final String key;
 
