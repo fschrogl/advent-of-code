503b26ebbf22ced32def94de4a8c23ddaa5e74e0
Fritz Schrogl <fschrogl@users.noreply.github.com>
2020-12-04 16:24:17 +0100

    Solved puzzle Day03 Part 2

--- src/main/java/at/schrogl/aoc/d03/Day03Application.java
+++ src/main/java/at/schrogl/aoc/d03/Day03Application.java
@@ -12,14 +12,21 @@ public class Day03Application {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         List<String> inputLines = getInputLines();
-        FlightMap flightMap = new FlightMap(inputLines);
-        flightMap.traverseFlightMap();
+        FlightMap flightMap = new FlightMap(inputLines, 0, 0);
 
-        System.out.println("## DAY 03: Part I");
+        System.out.println("## DAY 03: Part I + II\n");
         System.out.printf("Original flight map dimensions: %d x %d\n", flightMap.width(), flightMap.height());
         System.out.printf("Original flight map trees: %d\n", flightMap.treeCount());
-        System.out.printf("Original flight map none-trees: %d\n", (flightMap.width() * flightMap.height()) - flightMap.treeCount());
-        System.out.printf("Original flight map encountered trees: %d\n", flightMap.encounteredTrees());
+        System.out.printf("Original flight map none-trees: %d\n\n", (flightMap.width() * flightMap.height()) - flightMap.treeCount());
+
+        long totalEncounteredTrees = 1;
+        int[][] slopes = {{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
+        for (int[] slope : slopes) {
+            long encounteredTrees = getEncounteredTrees(inputLines, slope[0], slope[1]);
+            System.out.printf("Original flight map encountered trees (right=%d, down=%d): %d\n", slope[0], slope[1], encounteredTrees);
+            totalEncounteredTrees *= encounteredTrees;
+        }
+        System.out.printf("\nTotal encountered trees: %d", totalEncounteredTrees);
     }
 
     private static List<String> getInputLines() throws IOException, URISyntaxException {
@@ -28,21 +35,31 @@ public class Day03Application {
         }
     }
 
+    private static long getEncounteredTrees(List<String> inputLines, int xIncrement, int yIncrement) {
+        FlightMap flightMap = new FlightMap(inputLines, xIncrement, yIncrement);
+        flightMap.traverseFlightMap();
+        return flightMap.encounteredTrees();
+    }
+
     private static class FlightMap {
 
         private static final char TREE = '#';
         private final char[][] map;
+        private final int posXincrement;
+        private final int posYincrement;
 
         private long treeCount;
         private long encounteredTrees;
 
-        public FlightMap(List<String> inputLines) {
+        public FlightMap(List<String> inputLines, int posXincrement, int posYinrement) {
             this.map = new char[inputLines.size()][inputLines.get(0).trim().length()];
             for (int i = 0; i < inputLines.size(); i++) {
                 String line = inputLines.get(i).trim();
                 map[i] = line.toCharArray();
                 treeCount += line.chars().filter(c -> (c == TREE)).count();
             }
+            this.posXincrement = posXincrement;
+            this.posYincrement = posYinrement;
         }
 
         public int width() {
@@ -63,13 +80,13 @@ public class Day03Application {
 
         public void traverseFlightMap() {
             int posX = 0;
-            int posY = 1;
+            int posY = posYincrement;
 
             do {
-                posX += 3;
+                posX += posXincrement;
                 posX = (posX >= map[0].length) ? (posX - map[0].length) : posX;
                 encounteredTrees += (map[posY][posX] == TREE) ? 1 : 0;
-                posY++;
+                posY += posYincrement;
             } while (posY < map.length);
         }
     }
