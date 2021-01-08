7373da3372bb2f870c95d14105196d6cedad7a53
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-08 17:52:48 +0100

    WIP Day 17 Part I

---
+++ src/main/java/at/schrogl/aoc/d17/Day17.java
@@ -0,0 +1,36 @@
+package at.schrogl.aoc.d17;
+
+import at.schrogl.aoc.common.AbstractSolution;
+import at.schrogl.aoc.common.DayInfo;
+import at.schrogl.aoc.common.SolutionData;
+
+public class Day17 extends AbstractSolution {
+    @Override
+    public DayInfo getDayInfo() {
+        return new DayInfo(17, "Conway Cubes");
+    }
+
+    @Override
+    protected SolutionData example1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData example2() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise2() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day17().execute();
+    }
+}
