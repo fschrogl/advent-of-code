121abf4a893c0cfe25687e9aa499bf55e1dd2d24
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-09 02:22:54 +0100

    WIP Day 15 Part I

---
+++ src/main/java/at/schrogl/aoc/d15/Day15.java
@@ -0,0 +1,36 @@
+package at.schrogl.aoc.d15;
+
+import at.schrogl.aoc.common.AbstractSolution;
+import at.schrogl.aoc.common.DayInfo;
+import at.schrogl.aoc.common.SolutionData;
+
+public class Day15 extends AbstractSolution {
+    @Override
+    public DayInfo getDayInfo() {
+        return new DayInfo(15, "Rambunctious Recitation");
+    }
+
+    @Override
+    protected SolutionData example1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData example2() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise2() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day15().execute();
+    }
+}
