8214bb822fc0df0acde851c4086de7950c3e8cf1
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-10 15:09:07 +0100

    WIP Day 15 Part I

--- src/main/java/at/schrogl/aoc/d15/Day15Part1Solver.java
+++ src/main/java/at/schrogl/aoc/d15/Day15Part1Solver.java
@@ -1,8 +1,27 @@
 package at.schrogl.aoc.d15;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day15Part1Solver {
 
     public static long solve(String formula) {
-        return 0;
+        Map<Long, Integer> numberOccurrenceIndex = new HashMap<>();
+        String[] startingNumbers = formula.split(",");
+        Long lastNumberSpoken = null;
+
+        for (int i = 0; i < startingNumbers.length; i++) {
+            lastNumberSpoken = Long.parseLong(startingNumbers[i]);
+            numberOccurrenceIndex.put(lastNumberSpoken, i);
+        }
+
+        for (int i = startingNumbers.length; i < 10; ) {
+            Integer indexLastNumberSpokenOccurrence = numberOccurrenceIndex.get(lastNumberSpoken);
+            lastNumberSpoken = (indexLastNumberSpokenOccurrence <= 2) ? 0L : (long) (i - indexLastNumberSpokenOccurrence);
+            numberOccurrenceIndex.put(lastNumberSpoken, i);
+            i++;
+        }
+
+        return lastNumberSpoken;
     }
 }
