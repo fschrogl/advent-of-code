05cb35f9c4a1e8bc780aa683790d2c231a2449ac
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-10 15:21:13 +0100

    WIP Day 19 Part I

---
+++ src/main/java/at/schrogl/aoc/d19/Day19.java
@@ -0,0 +1,36 @@
+package at.schrogl.aoc.d19;
+
+import at.schrogl.aoc.common.AbstractSolution;
+import at.schrogl.aoc.common.DayInfo;
+import at.schrogl.aoc.common.SolutionData;
+
+public class Day19 extends AbstractSolution {
+    @Override
+    public DayInfo getDayInfo() {
+        return new DayInfo(19, "Monster Messages");
+    }
+
+    @Override
+    protected SolutionData example1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData example2() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise2() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day19().execute();
+    }
+}
