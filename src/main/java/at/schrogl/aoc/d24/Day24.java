60962f610327140880b79648197909da0e6f0f35
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-10 16:35:03 +0100

    WIP Day 24 Part I

---
+++ src/main/java/at/schrogl/aoc/d24/Day24.java
@@ -0,0 +1,36 @@
+package at.schrogl.aoc.d24;
+
+import at.schrogl.aoc.common.AbstractSolution;
+import at.schrogl.aoc.common.DayInfo;
+import at.schrogl.aoc.common.SolutionData;
+
+public class Day24 extends AbstractSolution {
+    @Override
+    public DayInfo getDayInfo() {
+        return new DayInfo(24, "Lobby Layout");
+    }
+
+    @Override
+    protected SolutionData example1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData example2() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise2() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day24().execute();
+    }
+}
