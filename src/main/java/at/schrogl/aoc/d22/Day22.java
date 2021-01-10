38e440f9b0f1cd04b889e95c188e85e962606896
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-10 16:27:42 +0100

    WIP Day 22 Part I

---
+++ src/main/java/at/schrogl/aoc/d22/Day22.java
@@ -0,0 +1,36 @@
+package at.schrogl.aoc.d22;
+
+import at.schrogl.aoc.common.AbstractSolution;
+import at.schrogl.aoc.common.DayInfo;
+import at.schrogl.aoc.common.SolutionData;
+
+public class Day22 extends AbstractSolution {
+    @Override
+    public DayInfo getDayInfo() {
+        return new DayInfo(22, "Crab Combat");
+    }
+
+    @Override
+    protected SolutionData example1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData example2() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise2() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day22().execute();
+    }
+}
