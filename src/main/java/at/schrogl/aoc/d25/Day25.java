96345d0d8d3d31aafb6ffe406d6f10cbb7120a0c
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-10 16:39:10 +0100

    WIP Day 25 Part I

---
+++ src/main/java/at/schrogl/aoc/d25/Day25.java
@@ -0,0 +1,36 @@
+package at.schrogl.aoc.d25;
+
+import at.schrogl.aoc.common.AbstractSolution;
+import at.schrogl.aoc.common.DayInfo;
+import at.schrogl.aoc.common.SolutionData;
+
+public class Day25 extends AbstractSolution {
+    @Override
+    public DayInfo getDayInfo() {
+        return new DayInfo(25, "Combo Breaker");
+    }
+
+    @Override
+    protected SolutionData example1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData example2() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise2() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day25().execute();
+    }
+}
