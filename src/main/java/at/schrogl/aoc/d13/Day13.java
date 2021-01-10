9d0e70eb605097031944f9feac0f2ae3a12b18ee
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-01-10 15:53:02 +0100

    WIP Day 13 Part I

---
+++ src/main/java/at/schrogl/aoc/d13/Day13.java
@@ -0,0 +1,36 @@
+package at.schrogl.aoc.d13;
+
+import at.schrogl.aoc.common.AbstractSolution;
+import at.schrogl.aoc.common.DayInfo;
+import at.schrogl.aoc.common.SolutionData;
+
+public class Day13 extends AbstractSolution {
+    @Override
+    public DayInfo getDayInfo() {
+        return new DayInfo(13, "Shuttle Search");
+    }
+
+    @Override
+    protected SolutionData example1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData example2() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise1() {
+        return null;
+    }
+
+    @Override
+    protected SolutionData exercise2() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new Day13().execute();
+    }
+}
